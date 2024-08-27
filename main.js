f7689818dbf51aea217d22b9362038584a4cc659
DannJohnrem <jonrhem10@gmail.com>
2024-08-28 00:38:46 +0800

    list rendering of data

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Hello Johnrem',
-            description: 'This an app that renders the data into the html'
+            product: 'Socks',
+            image: './assets/images/socks_green.jpg',
+            inventory: 9,
+            onSale: false,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                { id: 716, color: 'green' },
+                { id: 717, color: 'blue' }
+            ],
+            sizes: ['S', 'M', 'L', 'XL'],
         }
     }
 })
