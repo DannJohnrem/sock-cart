f4fbb45bc218744ae0bfae9192e90be17db65759
DannJohnrem <jonrhem10@gmail.com>
2024-08-28 17:05:22 +0800

    add: disabled when the item socks is out of stock

--- main.js
+++ main.js
@@ -3,14 +3,28 @@ const app = Vue.createApp({
         return {
             product: 'Socks',
             image: './assets/images/socks_green.jpg',
-            inventory: 9,
+            inventory: 2,
             onSale: false,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                { id: 716, color: 'green' },
-                { id: 717, color: 'blue' }
+                { id: 716, color: 'green', image: './assets/images/socks_green.jpg' },
+                { id: 717, color: 'blue' , image: './assets/images/socks_blue.jpg' }
             ],
             sizes: ['S', 'M', 'L', 'XL'],
+            cart: 0,
+        }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        removeFromCart() {
+            if (this.cart >= 1) {
+                this.cart -= 1
+            }
+        },
+        updateImage(variantImage) {
+            this.image = variantImage
         }
     }
 })
